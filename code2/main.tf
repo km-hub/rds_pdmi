6c1f828fa66116cc2aeff03d25e0e31f32da5bdd
ketan Muley <k25muley@gmail.com>
2020-10-27 14:38:29 +0530

    commit 4

--- code2/main.tf
+++ code2/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "my-test-sql" {
-  instance_class          = "${var.db_instance}"
+  instance_class          = var.db_instance
   engine                  = "mysql"
   engine_version          = "5.7"
   multi_az                = true
@@ -15,7 +15,7 @@ resource "aws_db_instance" "my-test-sql" {
   apply_immediately       = "true"
   backup_retention_period = 10
   backup_window           = "09:46-10:16"
-  db_subnet_group_name    = "${aws_db_subnet_group.my-rds-db-subnet.name}"
+  db_subnet_group_name    = aws_db_subnet_group.my-rds-db-subnet.name
   vpc_security_group_ids  = ["${aws_security_group.my-rds-sg.id}"]
 }
 
@@ -32,7 +32,7 @@ resource "aws_security_group" "my-rds-sg" {
 resource "aws_security_group_rule" "my-rds-sg-rule" {
   from_port         = 3306
   protocol          = "tcp"
-  security_group_id = "${aws_security_group.my-rds-sg.id}"
+  security_group_id = aws_security_group.my-rds-sg.id
   to_port           = 3306
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
@@ -41,7 +41,7 @@ resource "aws_security_group_rule" "my-rds-sg-rule" {
 resource "aws_security_group_rule" "outbound_rule" {
   from_port         = 0
   protocol          = "-1"
-  security_group_id = "${aws_security_group.my-rds-sg.id}"
+  security_group_id = aws_security_group.my-rds-sg.id
   to_port           = 0
   type              = "egress"
   cidr_blocks       = ["0.0.0.0/0"]
